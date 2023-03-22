f862b019729b2e01af50366dcabfe3879edb0b60
tonyfverdu <tony_verdu@hotmail.com>
2023-03-22 11:16:12 +0100

    commit ini

---
+++ Scripts/faceImage.js
@@ -0,0 +1,2 @@
+import { imageUpload, contImage, img} from "./initialitation.js"
+
---
+++ Scripts/initialitation.js
@@ -0,0 +1,3 @@
+export const imageUpload = document.querySelector("#imageUpload")
+export const contImage = document.querySelector(".contImage")
+export const img = document.querySelector("#imgCentral")
